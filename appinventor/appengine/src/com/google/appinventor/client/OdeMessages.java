58a5c06178512b33ced5bac7eef7c81a15877f5f
Lujing Cen <lujing@mit.edu>
2018-10-08 20:45:55 -0400

    working gql

--- appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
+++ appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
@@ -7081,9 +7081,9 @@ public interface OdeMessages extends Messages {
   @Description("")
   String gqlQueryParams();
 
-  @DefaultMessage("operationName")
+  @DefaultMessage("queryName")
   @Description("")
-  String gqlOperationNameParams();
+  String gqlQueryNameParams();
 
   @DefaultMessage("response")
   @Description("")
